c6d4b698c0239ee02ad43d5a269e870e96ad1656
Osman Dolu, Ph.D <osmandolu@gmail.com>
2022-11-04 00:29:03 +0300

    updated files
    file updates with pages

--- pages/3_Contact.py
+++
@@ -1,7 +0,0 @@
-
-"You can reach me with the following information:"
-"**MY CONTACT INFO:**"
-"Osman Dolu, Ph.D."
-"LinkedIn : https://www.linkedin.com/in/odolu/"
-"E-mail   : osmandolu@gmail.com"
-"GitHub   : www.github.com/osmandolu"
