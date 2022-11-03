9ef6becaa6ca95a8ee4dfc56aa2d076b1e3f2b07
Osman Dolu, Ph.D <101890403+osmandolu@users.noreply.github.com>
2022-11-04 02:51:11 +0300

    Update 2_📈_Quick_EDA.py

--- pages/2_📈_Quick_EDA.py
+++ pages/2_📈_Quick_EDA.py
@@ -1,14 +1,10 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from pandas_profiling import ProfileReport
+
 from streamlit_pandas_profiling import st_profile_report
 
 
 df = pd.read_csv("https://github.com/osmandolu/Streamlit/blob/main/final_scout_not_dummy.csv")
 pr = ProfileReport(df, explorative=True)
-# st.header('DataFrame Header')
-# st.write(df)
-# st.write('---')
-# st.header('Pandas Profiling Report')
 st_profile_report(pr)
