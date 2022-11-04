e8b4403879c1beea89f7ec0f55ae41ad75d29069
Osman Dolu, Ph.D <101890403+osmandolu@users.noreply.github.com>
2022-11-04 04:55:54 +0300

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -13,7 +13,7 @@ st.title("Welcome to No-Code Machine Learning with Streamlit!")
 "Hello visitor!" 
 "Welcome to my No-Code Machine Learning page!" 
 "My name is Osman Dolu. I am a data scientist with 10+ years of experience in this field."
-"With this web app, you will see a brief presentation of car price prediction using AutoScout data scraped from the Europe's famous car sale website. Instead of writing so many lines of code, you can simply change the parameters and see how the price of the car changes. Beware that the data I have used might not be up-to-date when you visit this page. 
+"With this web app, you will see a brief presentation of car price prediction using AutoScout data scraped from the Europe's famous car sale website. Instead of writing so many lines of code, you can simply change the parameters and see how the price of the car changes. Beware that the data I have used might not be up-to-date when you visit this page."
 "I have used Streamlit and Python's Numpy and Pandas libraries and major machine learning algorithms to realize this project. It's just a sample and in this respect, it should be noted as a showcase example for what can be done using Streamlit's powerful environment."
 "I hope you will like it."
 
